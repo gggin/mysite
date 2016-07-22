a390dafa6f0e3525f8a4f51577a050a160fb3c3b
Gin Liu <fm369o802340@163.com>
2016-07-22 21:34:11 +0800

    base

--- src/server.js
+++ src/server.js
@@ -4,6 +4,27 @@
 
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+var config = require('./config');
+mongoose.connect(config.DB_PATH);
+
+// parse application/json
+var jsonParser = bodyParser.json();
+router.use(bodyParser.json());
+
+var User = mongoose.model('User', schemas.UserSchema);
+
+app.use(session({
+    secret: config.ADMIN_SECRECT,
+    store: new MongoStore({
+        url: config.DB_PATH
+    })
+}));
 
 var admin = require('./admin/user.js');
 app.use("/admin", admin);
